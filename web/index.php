cea0877f0cd37e07d2f25861b37842c343424099
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-27 15:11:22 +0100

    Session handler test

--- web/index.php
+++ web/index.php
@@ -5,9 +5,11 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Framework\Application;
 use Framework\Myy;
+use Framework\Session;
 
-try {
-    $app = new Application($config) ?? null;
+//try {
+$app = new Application($config) ?? null;
+/*
 } catch (\Throwable $th) {
     if (Application::hasValidParam($config, 'debug') && $config['params']['debug']) {
         (new Pages\Error(500, $th->getMessage()))->render();
@@ -16,7 +18,17 @@ try {
     }
     exit(500);
 }
-
+*/
 Myy::init();
 Myy::$app = $app;
 Myy::$user = null;
+
+$app->setSession(new Session());
+
+$app->getSession()->set('hello', 'world');
+var_dump($_SESSION, $app->getSession()->getName(), $app->getSession()->getId(), SID);
+echo "after shutdown \n";
+$app->getSession()->close();
+unset($app);
+$app = null;
+var_dump($_SESSION, SID);
