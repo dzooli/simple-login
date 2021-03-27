1eafcbc62f4bb87a27545a443160dae0b22775d5
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-27 14:01:09 +0100

    Simple error page added

---
+++ src/pages/Error.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pages;
+
+use \Framework\SimpleView;
+
+class Error extends SimpleView
+{
+    public function __construct(int $code, string $message)
+    {
+        $this->title = 'Error Page';
+        $this->content = <<<ENDCONTENT
+            <div class="w3-panel w3-red w3-round w3-card-4">
+            <H1>Error: {$code}</H1>
+            <H2>Message: {$message}</H2>
+            </div>
+ENDCONTENT;
+    }
+}
