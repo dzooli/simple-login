4ca04362ad1a43a88065969ae79c153ad5628493
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-28 01:44:49 +0100

    Use of Myy to handle the app lifecycle (with exception handling)

--- web/index.php
+++ web/index.php
@@ -3,32 +3,20 @@
 $config = require_once(__DIR__ . '/../config/app.php');
 require __DIR__ . '/../vendor/autoload.php';
 
-use Framework\Application;
 use Framework\Myy;
-use Framework\Session;
 
-//try {
-$app = new Application($config) ?? null;
-/*
+Myy::init();
+
+try {
+    Myy::createApplication($config);
+    echo Myy::$app->run()->getContent();
 } catch (\Throwable $th) {
-    if (Application::hasValidParam($config, 'debug') && $config['params']['debug']) {
+    if (Myy::$app->isDebugging()) {
         (new Pages\Error(500, $th->getMessage()))->render();
     } else {
         header('HTTP/1.0 500 Internal Server Error');
     }
     exit(500);
+} finally {
+    Myy::shutdownApplication();
 }
-*/
-Myy::init();
-Myy::$app = $app;
-Myy::$user = null;
-
-$app->setSession(new Session());
-
-$app->getSession()->set('hello', 'world');
-var_dump($_SESSION, $app->getSession()->getName(), $app->getSession()->getId(), SID);
-echo "after shutdown \n";
-$app->getSession()->close();
-unset($app);
-$app = null;
-var_dump($_SESSION, SID);
