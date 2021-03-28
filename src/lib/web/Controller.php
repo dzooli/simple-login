4bc2cba950f7fc3875bb339b2545a986089acf6c
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-28 23:51:45 +0200

    Added renderView(), login() and logout()

--- src/lib/web/Controller.php
+++ src/lib/web/Controller.php
@@ -2,7 +2,11 @@
 
 namespace Framework\Web;
 
+use Framework\Myy;
 use Framework\Exception\InvalidRedirectTargetException;
+use Framework\Exception\ViewNotFoundException;
+use Framework\Session;
+use Framework\UserBase;
 
 class Controller
 {
@@ -21,6 +25,7 @@ class Controller
 
     public function getName(): string
     {
+
         $names = explode('\\', $this->name);
         if (!empty($names)) {
             return $names[count($names) - 1];
@@ -39,8 +44,40 @@ class Controller
         $result->addHeader(
             'Location: /' . $controller_and_action
                 . (!empty($params) ? '?' : '')
-                . http_build_query($params)
+                . (!empty($params) ? http_build_query($params) : '')
         );
+        $result->setRedirect();
         exit();
+    }
+
+    public function renderView(?string $viewName = null, ?array $params = null): string
+    {
+        $viewFile = __DIR__ . '/../../app/views/'
+            . strtolower(str_replace('Controller', '', $this->getName()))
+            . '/' . ((!empty($viewName)) ? strtolower($viewName) : 'index')
+            . '.php';
+        if (!file_exists($viewFile)) {
+            throw new ViewNotFoundException($this->name, $viewFile);
+        }
+        ob_start();
+        require($viewFile);
+        return ob_get_clean();
+    }
+
+    protected function login(?UserBase $user = null)
+    {
+        if (!empty($user) && $user->getId() > 0) {
+            if (empty(Myy::$app->getSession())) {
+                Myy::$app->setSession(new Session());
+            }
+            Myy::$app->getSession()->set('user_id', $user->getId());
+            Myy::$user_id = $user->getId();
+        }
+    }
+
+    protected function logout()
+    {
+        Myy::$app->getSession()->close();
+        Myy::$user_id = 0;
     }
 }
