a59e9a581340a2625f6dd9a34ba8553634806675
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-28 23:51:01 +0200

    complete redesign of UserBase

--- src/lib/UserBase.php
+++ src/lib/UserBase.php
@@ -2,12 +2,37 @@
 
 namespace Framework;
 
-abstract class UserBase
+class UserBase
 {
-    abstract public function getName(): string;
-    abstract public function getId(): int;
-    abstract public function authenticate(): bool;
-    abstract protected function checkPassword(): bool;
-    abstract public function getRoles(): array;
-    abstract public function hasRoleByName(string $roneName): bool;
+    protected int $id = 0;
+    protected string $password = '';
+    protected string $name = '';
+
+    public function __construct(int $id = 0)
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getName(): string
+    {
+        return $this->username;
+    }
+
+    protected static function preCheck(): ?bool
+    {
+        if (empty(Myy::$app) || empty(Myy::$app->getDb())) {
+            throw new NotInitializedException();
+        }
+        return true;
+    }
 }
