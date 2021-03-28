79e155bd84ea0769e41f543ffcf82e92be24031f
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-28 01:38:49 +0100

    added createApplication(), shutdownApplication(), isGuest()

--- src/lib/Myy.php
+++ src/lib/Myy.php
@@ -3,6 +3,7 @@
 namespace Framework;
 
 use Framework\Application;
+use Framework\Session;
 use Framework\UserBase;
 
 class Myy
@@ -15,5 +16,25 @@ class Myy
     public static function init(): void
     {
         self::$app = self::$user = null;
+    }
+
+    public static function isGuest(): bool
+    {
+        return (self::$user === null);
+    }
+
+    public static function createApplication(?array $config): void
+    {
+        self::$app = new Application($config);
+        if (self::$app->isDebugging()) {
+            ini_set('max_execution_time', 300);
+        }
+        self::$app->setSession(new Session());
+    }
+
+    public static function shutdownApplication(): void
+    {
+        self::$app->getSession()->close();
+        self::$app = null;
     }
 }
