eb03a44b7f2d0cc44d53c82393eab951207713fb
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-30 00:15:44 +0200

    some additional tables (not complete yet)

--- db/create_sampledb.sql
+++ db/create_sampledb.sql
@@ -19,4 +19,24 @@ CREATE TABLE `laureldb`.`user` (
 
 INSERT INTO `laureldb`.`user` (`username`, `email`, `password`) VALUES ('admin', 'admin@email.com', '$2y$10$JU8h7esuD5rvqvBsXbrdz.yp4iq3PnbqsJTkGbap3yb6K.cYVT3Om');
 INSERT INTO `laureldb`.`user` (`username`, `email`, `password`) VALUES ('user', 'user@email.com', '$2y$10$vzMIpJsB8MQxYYCi.Y1VX.G4xPH8ZO79updhNuw.c7e9pxu2QsCRi');
-  
+
+CREATE TABLE `laureldb`.`role` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(20) NOT NULL COMMENT 'Role Name',
+  `description` VARCHAR(255) NULL COMMENT 'Role Description',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+INSERT INTO `laureldb`.`role` (`name`, `description`) VALUES ('admin', 'Administrator');
+INSERT INTO `laureldb`.`role` (`name`, `description`) VALUES ('user', 'Normal user');
+INSERT INTO `laureldb`.`role` (`name`, `description`) VALUES ('operator', 'Operational user');
+
+CREATE TABLE `laureldb`.`page` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL COMMENT 'Page Name',
+  `stylesheet` VARCHAR(50) NULL COMMENT 'Used Stylesheet',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+COMMENT = 'Available pages to show';
+
