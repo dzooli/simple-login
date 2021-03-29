3f9d3b999475f97c879c6a936ab5bc06dbc37faa
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-30 00:21:49 +0200

    Added flash display and content display

--- src/app/views/site/starter.php
+++ src/app/views/site/starter.php
@@ -4,6 +4,9 @@
 
     use Framework\Myy;
     use App\Models\User;
+    use Framework\Session;
+
+    $flash = Session::getFlash();
     ?>
 
     <html lang="en">
@@ -28,8 +31,23 @@
         </div>
         <!-- Navbar end -->
 
+        <?php if ($flash) : ?>
+            <div class="w3-card w3-<?= $flash['color'] ?> w3-display-container w3-top w3-animate-zoom">
+                <span onclick="this.parentElement.style.display='none'" class="w3-button w3-large w3-<?= $flash['color'] ?> w3-display-topright">×</span>
+                <div class="w3-container">
+                    <p><?= $flash['msg'] ?></p>
+                </div>
+            </div>
+        <?php endif ?>
+
         <div class="w3-content w3-container">
             <h1>Welcome Home!</h1>
+            <div class="w3-container">
+                <?php $pagename = __DIR__ . '/' . $page . '.html';
+                if (!empty($page) && file_exists($pagename)) : ?>
+                    <?php require_once($pagename); ?>
+                <?php endif ?>
+            </div>
         </div>
     </body>
 
