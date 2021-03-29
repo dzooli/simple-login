ccc5b091cbafa1309aebb19a89e2a88b980def3e
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-30 00:20:37 +0200

    UserPage is the main query for allowed pages of the users

---
+++ src/app/models/UserPage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use \PDO;
+use Framework\Myy;
+use Framework\Model;
+
+class UserPage extends Model
+{
+
+    protected array $pages = [];
+
+    public static function findPagesFor(int $id = 0): ?array
+    {
+        parent::preCheck();
+        $dbconn = Myy::$app->getDb();
+
+        $stmt = $dbconn->prepare('SELECT DISTINCT
+    pag.name
+FROM
+    user u
+        LEFT JOIN
+    user_has_role ur ON u.id = ur.user_id
+        LEFT JOIN
+    role rol ON ur.role_id = rol.id
+        LEFT JOIN
+    role_has_page rp ON rol.id = rp.role_id
+        LEFT JOIN
+    page pag ON rp.page_id = pag.id
+WHERE
+    u.id = :user_id;');
+
+        $dbDone = $stmt->execute([':user_id' => $id]);
+        if ($dbDone) {
+            $res = $stmt->fetchAll(PDO::FETCH_COLUMN | PDO::FETCH_UNIQUE, 0);
+        }
+        return (!empty($res) || is_array($res)) ? array_values($res) : null;
+    }
+}
