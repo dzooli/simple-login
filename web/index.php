1548d77a67b1b42e34f1eb2930804926517ddc99
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-30 00:24:32 +0200

    Handler for 404 error (more user friendly)

--- web/index.php
+++ web/index.php
@@ -3,6 +3,8 @@
 $config = require_once(__DIR__ . '/../config/app.php');
 require __DIR__ . '/../vendor/autoload.php';
 
+use Framework\Exception\ActionNotFoundException;
+use Framework\Exception\ControllerNotFoundException;
 use Framework\Myy;
 
 Myy::init();
@@ -12,6 +14,14 @@ try {
     ob_start();
     echo Myy::$app->run()->getContent();
     ob_end_flush();
+} catch (ActionNotFoundException $ex) {
+    (new Pages\Error404())->render();
+    header('HTTP/1.0 404 Not Found');
+    exit(404);
+} catch (ControllerNotFoundException $ex) {
+    (new Pages\Error404())->render();
+    header('HTTP/1.0 404 Not Found');
+    exit(404);
 } catch (\Throwable $th) {
     if (Myy::$app->isDebugging()) {
         (new Pages\Error(500, $th->getMessage()))->render();
