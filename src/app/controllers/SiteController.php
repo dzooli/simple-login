c87161a78743224261f060d1514b703a80652e40
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-30 00:17:26 +0200

    Added UserPage search and display for the site/starter action

--- src/app/controllers/SiteController.php
+++ src/app/controllers/SiteController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controllers;
 
+use App\Models\UserPage;
+
+use Framework\Myy;
+use Framework\Session;
 use Framework\Web\Response;
 use Framework\Web\Controller;
 
@@ -9,11 +13,23 @@ class SiteController extends Controller
 {
     public function actionIndex(): Response
     {
+        if (!Myy::isGuest()) {
+            return new Response($this->localRedirect('site/starter'));
+        }
         return new Response($this->renderView('index'));
     }
 
     public function actionStarter(): Response
     {
-        return new Response($this->renderView('starter'));
+        if (!Myy::isGuest()) {
+            $pages = UserPage::findPagesFor(Myy::$user_id);
+            $nopages = empty($pages) || empty($pages[0]);
+            if ($nopages) {
+                Session::setFlash('warning', 'No pages available for you!');
+            }
+            return new Response($this->renderView('starter', $nopages ? ['page' => ''] : ['page' => $pages[rand(0, count($pages) - 1)]]));
+        }
+        Session::setFlash('danger', 'You are out of the basket. Please sign in first.');
+        return new Response($this->renderView('index'));
     }
 }
