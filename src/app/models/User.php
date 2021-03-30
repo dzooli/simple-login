0e16764028ace85f8a80fd9b72bea98fa352ed45
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-30 23:19:09 +0200

    Find only the first matching email and docblocks added

--- src/app/models/User.php
+++ src/app/models/User.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use \Framework\UserBase;
 use \Framework\Myy;
 
+/**
+ * The Application specific User class
+ * 
+ * Could be adjusted to the specified requirements
+ */
 class User extends UserBase
 {
     protected int $id = 0;
@@ -15,12 +20,23 @@ class User extends UserBase
     protected $last_login = 0;
     protected bool $logged_in = false;
 
+    /**
+     * The constructor
+     * 
+     * In this application the constructor is only used to set the types of some properties 
+     */
     public function __construct()
     {
         settype($this->id, 'integer');
         settype($this->logged_in, 'bool');
     }
 
+    /**
+     * Find the user by e-mail address
+     *
+     * @param string $email
+     * @return UserBase|null    The first row as object instance from `user` table with the matching e-mail address
+     */
     public static function findByEmail(string $email = ''): ?UserBase
     {
         if (empty($email)) {
@@ -28,7 +44,7 @@ class User extends UserBase
         }
         parent::preCheck();
         $dbconn = Myy::$app->getDb();
-        $stmt = $dbconn->prepare('SELECT * FROM `user` WHERE email=:email_address');
+        $stmt = $dbconn->prepare('SELECT * FROM `user` WHERE email=:email_address ORDER BY id LIMIT 1');
         $dbDone = $stmt->execute([':email_address' => $email]);
         if ($dbDone) {
             $res = $stmt->fetchObject(__CLASS__);
@@ -36,6 +52,12 @@ class User extends UserBase
         return ($res instanceof UserBase) ? $res : null;
     }
 
+    /**
+     * Find the user by ID
+     *
+     * @param integer $id
+     * @return UserBase|null    The user object with the specified ID or null when not found
+     */
     public static function findById(int $id = 0): ?UserBase
     {
         parent::preCheck();
@@ -48,6 +70,15 @@ class User extends UserBase
         return ($res instanceof UserBase) ? $res : null;
     }
 
+    /**
+     * Verify the password
+     * 
+     * Compares the given $pass with the crypted hash in the DB.
+     * You can generate passwords with password_hash(..., PASSWORD_DEFAULT) function.
+     *
+     * @param string $pass  The password to verify
+     * @return boolean  True on match false otherwise
+     */
     public function verifyPassword($pass = null): bool
     {
         if (!$pass || empty($this->password)) {
