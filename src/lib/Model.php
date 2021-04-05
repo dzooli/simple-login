b0edd6c88880f03f1f1a2fbef0c22d647e4635fd
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-04-05 21:42:46 +0200

    Documentation improvements

--- src/lib/Model.php
+++ src/lib/Model.php
@@ -7,15 +7,19 @@ use Framework\Exception\NotInitializedException;
 
 /**
  * The base model
+ * 
+ * This is the Model base for all database realted models. It is usable 
+ * for extending to your domain specifice models regarding of the
+ * application requirements.
  */
 class Model
 {
 
     /**
-     * Check for Application and DB connecttion
+     * Check for Application initialization and DB connecttion
      *
      * @return boolean true if the application and the DB connection is exists
-     * @throws NotInitializedException if one of them is missing
+     * @throws NotInitializedException if one of the above is missing
      */
     protected static function preCheck(): ?bool
     {
